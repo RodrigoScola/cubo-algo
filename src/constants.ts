dc538f7f4f8b86052e7ae321e22121ead9234578
rodrigoscola <rodrigo.sgarabotto.scola@gmail.com>
2023-10-11 17:29:36 -0300

    dbcon

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ require("dotenv").config();
 
 export const __DEV__ = process.env.NODE_ENV === "development";
 
-export const DATABASE_URL = process.env.DATABASE_URL || "";
+export const DATABASE_URL = process.env.DATABASE_URL || "mysql://g8741qkaethxg2s7o7pr:pscale_pw_oryuHO0zkTD2ojqn4YBYUjGNbBXeqQwd2ILClaq7zjT@aws.connect.psdb.cloud/cube_database?ssl={"rejectUnauthorized":true}";
+
